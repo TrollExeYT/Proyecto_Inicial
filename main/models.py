8b19ec3cfcfa9313771aac4ec2b3c7d16fb3996c
Benjamin <bennacho97@outlook.cl>
2025-10-08 22:31:25 -0300

    Posibles tablas de la  base de datos

--- main/models.py
+++ main/models.py
@@ -1,28 +1,43 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+from django.forms import IntegerField
 
-# Create your models here.
+# Modelo de los usuarios
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=100)
 
+    USERNAME_FIELD = 'username'
+    PASSWORD_FIELD = 'password'
+
+    REQUIRED_FIELDS = ['username', 'password']
+
+# Modelo de los eventos predeterminados
 class Event(models.Model):
-    name = models.CharField(max_length=30, blank=False, null=False)
-    photo_path = models.TextField(blank=False, null=False)
+    name = models.CharField(max_length=30)
+    photo_path = models.CharField(max_length=150)
 
+    REQUIRED_FIELDS = ['name', 'photo_path']
 
+# Calendarios
 class Calendar(models.Model):
     name = models.CharField(max_length=30, blank=False, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    #photo = models.ImageField()
+    # Buscar manera de subir archivos para los calendarios o en caso contrario crear predeterminados
+    # photo = models.ImageField()
 
-"""
-class CalendarEvent(models.Model):
-    RANGE = range(0,3)
-    DIVISIONS = [
-        (0, "Dia"),
-        (1, "Tarde"),
-        (2, "Noche"),
-    ]
-    division = models.IntegerField(choices=DIVISIONS, default=0)
-    position = models.IntegerField(choices=RANGE, default=0)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    REQUIRED_FIELDS = ['user', 'name']
+
+# Conectores y divisores de eventos
+# Crear una view especifica para
+class EventConnector(models.Model):
+    DIVISIONS = (
+        (0, 'Dia'),
+        (1, 'Tarde'),
+        (2, 'Noche'),
+    )
+
+    division = models.IntegerField(choices=DIVISIONS)
+    subdivision = IntegerField(min_value=0, max_value=2)
     calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
-"""
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
 def test(request):
     return render(request, 'base.html')
+
+
+def add_event(request):
+    if request.method == "POST":
+        data = request.POST
+        if 0 < int(data["division"]) < 2 or 0 < int(data["subdivision"]) < 2:
+            try:
+                connection = EventConnector(
+                    calendar_id=request.POST['calendar_id'],
+                    event_id=request.POST['event_id'],
+                    division_id=request.POST['division'],
+                    subdivision_id=request.POST['subdivision'],
+                )
+                connection.save()
+            except Exception as e:
+                print(e)
+        else:
+            pass
+    else:
+        pass
