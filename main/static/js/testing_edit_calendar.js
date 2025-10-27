773d6e71c98727820e459800f65309022fe5e125
Benjamin <bennacho97@outlook.cl>
2025-10-27 14:19:48 -0300

    Se intento añadir drag and drop para moviles - B

--- main/static/js/testing_edit_calendar.js
+++ main/static/js/testing_edit_calendar.js
@@ -6,26 +6,47 @@ const eventsBlocks = document.querySelectorAll('.event-block');
 // Bloques en la agenda
 const calendarBlocks = document.querySelectorAll('.calendar-block');
 
-eventsBlocks.forEach((block) => block.addEventListener('dragstart', e => {
+eventsBlocks.forEach((block) => block.addEventListener('dragstart', () => {
     SetEvent(block.dataset.id);
 }))
 
-eventsBlocks.forEach((block) => block.addEventListener('click', e => {
+eventsBlocks.forEach((block) => block.addEventListener('click', () => {
     SetEvent(block.dataset.id);
 }))
+/*
+// SE INTENTO AÑADIR DRAG AND DROP PARA MOVILES (No se pudo)
+eventsBlocks.forEach((block) => block.addEventListener('touchstart', e => {
+    block.classList.add('in-movement');
 
+    let touchLocation = e.changedTouches[0];
+
+    block.style.left = `${touchLocation.pageX}px`;
+    block.style.top = `${touchLocation.pageY}px`;
+}))
+
+eventsBlocks.forEach((block) => block.addEventListener('touchmove', e => {
+    let touchLocation = e.changedTouches[0];
+
+    block.style.left = `${touchLocation.pageX}px`;
+    block.style.top = `${touchLocation.pageY}px`;
+}))
+
+eventsBlocks.forEach((block) => block.addEventListener('touchend', () => {
+    block.classList.remove('in-movement');
+}))
+*/
 calendarBlocks.forEach((block) => block.addEventListener('dragover', e => {
     e.preventDefault();
 }))
 
-calendarBlocks.forEach((block) => block.addEventListener('drop', e => {
+calendarBlocks.forEach((block) => block.addEventListener('drop', () => {
     let day = block.dataset.day;
     let group = block.dataset.group;
     let division = block.dataset.division;
     SetLocation(day, group, division);
 }))
 
-calendarBlocks.forEach((block) => block.addEventListener('click', e => {
+calendarBlocks.forEach((block) => block.addEventListener('click', () => {
     let day = block.dataset.day;
     let group = block.dataset.group;
     let division = block.dataset.division;
@@ -38,6 +59,7 @@ function SetEvent(id_event){
     event.setAttribute('value', String(id_event));
     eventSelected = true;
 }
+
 function SetLocation(day, group, division){
     // Selecionar la posicion en la que se va a enviar el evento
     document.getElementById('id_day').value = day;
