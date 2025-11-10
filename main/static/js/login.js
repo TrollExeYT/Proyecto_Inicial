ba18cb641309565ebed852028529ba1569412749
Benjamin <bennacho97@outlook.cl>
2025-11-10 20:55:47 -0300

    Mensajes de error en login - B

---
+++ main/static/js/login.js
@@ -0,0 +1,10 @@
+let errorsDiv = document.getElementById('error-div')
+
+if (errorsDiv.textContent.trim() !== '') {
+    let username = document.getElementById('username');
+    let password = document.getElementById('password');
+    username.classList.add('is-invalid');
+    password.classList.add('is-invalid');
+    console.log('kdsja');
+}
+
