09e76e87a19f1c05d52392d52570f2d190838d7c
Benjamin <bennacho97@outlook.cl>
2025-11-10 22:19:33 -0300

    Optimización js - B

--- main/static/js/testing_edit_calendar.js
+++ main/static/js/testing_edit_calendar.js
@@ -6,13 +6,10 @@ const eventsBlocks = document.querySelectorAll('.event-block');
 // Bloques en la agenda
 const calendarBlocks = document.querySelectorAll('.calendar-block');
 
-eventsBlocks.forEach((block) => block.addEventListener('dragstart', () => {
-    SetEvent(block.dataset.id);
-}))
-
-eventsBlocks.forEach((block) => block.addEventListener('click', () => {
-    SetEvent(block.dataset.id);
-}))
+eventsBlocks.forEach((block) => {
+    block.addEventListener('click', () => SetEvent(block.dataset.id));
+    block.addEventListener('dragstart', () => SetEvent(block.dataset.id));
+})
 /*
 // SE INTENTO AÑADIR DRAG AND DROP PARA MÓVILES (No se pudo)
 eventsBlocks.forEach((block) => block.addEventListener('touchstart', e => {
@@ -39,32 +36,23 @@ calendarBlocks.forEach((block) => block.addEventListener('dragover', e => {
     e.preventDefault();
 }))
 
-calendarBlocks.forEach((block) => block.addEventListener('drop', () => {
-    let day = block.dataset.day;
-    let group = block.dataset.group;
-    let division = block.dataset.division;
-    SetLocation(day, group, division);
-}))
+calendarBlocks.forEach((block) => {
+    block.addEventListener('drop', () => SetLocation(block));
+    block.addEventListener('click', e => SetLocation(block));
+})
 
-calendarBlocks.forEach((block) => block.addEventListener('click', () => {
-    let day = block.dataset.day;
-    let group = block.dataset.group;
-    let division = block.dataset.division;
-    SetLocation(day, group, division);
-}))
-
-function SetEvent(id_event){
+function SetEvent(id_event) {
     // Para seleccionar un evento
     let event = document.getElementById('id_event');
     event.setAttribute('value', String(id_event));
     eventSelected = true;
 }
 
-function SetLocation(day, group, division){
+function SetLocation(block) {
     // Seleccionar la posición en la que se va a enviar el evento
-    document.getElementById('id_day').value = day;
-    document.getElementById('id_group').value = group;
-    document.getElementById('id_division').value = division;
+    document.getElementById('id_day').value = block.dataset.day;
+    document.getElementById('id_group').value = block.dataset.group;
+    document.getElementById('id_division').value = block.dataset.division;
     // También se va a enviar el formulario
     SendForm();
 }
